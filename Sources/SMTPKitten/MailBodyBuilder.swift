64c99759c9718e9cf79cd7dc36742e6c49d1f77f
tierracero <vcantu01@tierracero.com>
2024-01-21 17:29:15 -0600

    Merge pull request #8 from skatbox/base64_limit
    fix base64 inline

--- Sources/SMTPKitten/MailBodyBuilder.swift
+++ Sources/SMTPKitten/MailBodyBuilder.swift
@@ -9,4 +9,14 @@ public struct MailBodyBuilder {
             return .multiple(blocks)
         }
     }
+    public static func buildBlock(_ components: [MailContentConvertible]) -> Mail.Content {
+
+        let blocks = components.flatMap(\.content.blocks)
+
+        if blocks.count == 1 {
+            return .single(blocks[0])
+        } else {
+            return .multiple(blocks)
+        }
+    }
 }
