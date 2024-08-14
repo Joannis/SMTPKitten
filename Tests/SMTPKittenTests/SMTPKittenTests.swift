4a229a3f34b138dad389545338c4f4cea9fe35bd
Joannis Orlandos <joannis@orlandos.nl>
2024-08-14 15:33:39 +0200

    Send a basic mail through MailHog

--- Tests/SMTPKittenTests/SMTPKittenTests.swift
+++ Tests/SMTPKittenTests/SMTPKittenTests.swift
@@ -1,1 +1,21 @@
+import XCTest
+import SMTPKitten
 
+final class SMTPKittenTests: XCTestCase {
+    func testBasics() async throws {
+        try await SMTPClient.withConnection(
+            to: "localhost",
+            port: 1025,
+            ssl: .insecure
+        ) { client in
+            try await client.sendMail(
+                Mail(
+                    from: MailUser(name: "Joannis", email: "joannis@unbeatable.software"),
+                    to: [MailUser(name: "MailHog User", email: "test@mail.hog")],
+                    subject: "Test mail",
+                    content: .plain("Hello world")
+                )
+            )
+        }
+    }
+}
