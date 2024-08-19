bada4b760176702d2d8792c323f58e9c9c46404b
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2024-08-19 16:57:38 -0400

    chore(CI): fixing tests for CI

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,13 +12,13 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SMTPKitten",
-            targets: ["SMTPKitten"]),
+            targets: ["SMTPKitten"])
     ],
     dependencies: [
         // 🚀
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        
+
         // 🔑
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
     ],
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SMTPKittenTests
-
-var tests = [XCTestCaseEntry]()
-tests += SMTPKittenTests.allTests()
-XCTMain(tests)
