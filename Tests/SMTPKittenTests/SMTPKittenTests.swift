53f51f10582d7669b0213aa335b3b2458c9e1a82
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2024-08-19 16:42:17 -0400

    chore(CI): fixing tests for CI

--- Tests/SMTPKittenTests/SMTPKittenTests.swift
+++ Tests/SMTPKittenTests/SMTPKittenTests.swift
@@ -1,9 +1,10 @@
-import XCTest
 import NIO
 import NIOFoundationCompat
-import NIOSSL
-@testable import SMTPKitten
 import NIOPosix
+import NIOSSL
+import XCTest
+
+@testable import SMTPKitten
 
 @available(macOS 13.0, *)
 final class SMTPKittenTests: XCTestCase {
@@ -11,17 +12,17 @@ final class SMTPKittenTests: XCTestCase {
         let environment = ProcessInfo.processInfo.environment
 
         guard
-            let hostname = environment["TEST_HOST"],
-            let username = environment["TEST_USERNAME"],
-            let password = environment["TEST_PASSWORD"],
+            let hostname = environment["SMTP_HOSTNAME"],
+            let username = environment["SMTP_USER"],
+            let password = environment["SMTP_PASSWORD"],
             let imagePath = environment["TEST_IMAGE_PATH"],
             let attachmentPath = environment["TEST_ATTACHMENT_PATH"]
         else {
             return XCTFail("Missing environment variables")
         }
 
-        let image = try Data(contentsOf: URL(filePath: imagePath))
-        let attachment = try Data(contentsOf: URL(filePath: attachmentPath))
+        let image = try Data(contentsOf: URL(fileURLWithPath: imagePath))
+        let attachment = try Data(contentsOf: URL(fileURLWithPath: attachmentPath))
 
         let mail = Mail(
             from: MailUser(name: "SMTPKitten Tester", email: username),
@@ -33,7 +34,7 @@ final class SMTPKittenTests: XCTestCase {
             Mail.Content.alternative("**End** of mail btw.", html: "<b>End</b> of mail btw.")
             Mail.Attachment(attachment, mimeType: "application/pdf")
         }
-        
+
         var tlsConfig = TLSConfiguration.clientDefault
         tlsConfig.certificateVerification = .none
         let client = try await SMTPClient.connect(
@@ -41,17 +42,17 @@ final class SMTPKittenTests: XCTestCase {
             port: 587,
             ssl: .startTLS(configuration: .default)
         )
-        
+
         try await client.login(
             user: username,
             password: password
         )
-        
+
         try await client.sendMail(mail)
     }
-    
+
     func testMutableContentExample() async throws {
-        
+
         let environment = ProcessInfo.processInfo.environment
 
         guard
@@ -64,16 +65,16 @@ final class SMTPKittenTests: XCTestCase {
             return XCTFail("Missing environment variables")
         }
 
-        let image = try Data(contentsOf: URL(filePath: imagePath))
-        let attachment = try Data(contentsOf: URL(filePath: attachmentPath))
+        let image = try Data(contentsOf: URL(fileURLWithPath: imagePath))
+        let attachment = try Data(contentsOf: URL(fileURLWithPath: attachmentPath))
 
-        var contents: [MailContentConvertible] = [
+        let contents: [MailContentConvertible] = [
             "Welcome to our app, you're all set up & stuff.",
             Mail.Image.png(image, filename: "Screenshot.png"),
             Mail.Content.alternative("**End** of mail btw.", html: "<b>End</b> of mail btw."),
-            Mail.Attachment(attachment, mimeType: "application/pdf")
+            Mail.Attachment(attachment, mimeType: "application/pdf"),
         ]
-        
+
         let mail = Mail(
             from: MailUser(name: "SMTPKitten Tester", email: username),
             to: ["joannis@unbeatable.software"],
@@ -81,7 +82,7 @@ final class SMTPKittenTests: XCTestCase {
         ) {
             contents
         }
-        
+
         var tlsConfig = TLSConfiguration.clientDefault
         tlsConfig.certificateVerification = .none
         let client = try await SMTPClient.connect(
@@ -89,12 +90,12 @@ final class SMTPKittenTests: XCTestCase {
             port: 587,
             ssl: .startTLS(configuration: .default)
         )
-        
+
         try await client.login(
             user: username,
             password: password
         )
-        
+
         try await client.sendMail(mail)
     }
 }
